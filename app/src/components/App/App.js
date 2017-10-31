ee79f9b17ffd4ac772ddf0b17eb3211d09474c2c
Scott Braia <scottbraia@gmail.com>
2017-10-31 16:28:44 -0400

    Progress up to number 28

---
+++ app/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            {/*Add a SearchResults component*/}
+            {/*Add a Playlist component*/}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
